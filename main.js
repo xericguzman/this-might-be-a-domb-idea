91459cca7edb2ae344b3abf712a6b446faa14cf9
eric guzman <xericguzman@gmail.com>
2021-11-01 00:45:18 -0400

    si

---
+++ main.js
@@ -0,0 +1,18 @@
+const paragraph = document.querySelector("p");
+paragraph.style.color = "lightblue";
+
+const h1 = document.querySelector("h1");
+h1.style.fontSize = "48px";
+paragraph.innerText =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur necessitatibus quod nesciunt esse, corporis aliquam ullam deleniti dolore neque autem! Nihil tempora impedit earum perferendis pariatur molestiae sequi possimus?";
+const item13 = document.querySelector("#item-13");
+item13.style.opacity = 0.5;
+document.getElementById("item-3").innerText = 'I say, "Hi"';
+const image = document.querySelector("img");
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+image.style.height = "300px";
+const cactus = document.querySelector(".cactus");
+cactus.style.height = "300px";
+const item16 = document.querySelector("#item-16");
+item16.className = "item";
+item16.innerText = "Won't get fooled again.";
